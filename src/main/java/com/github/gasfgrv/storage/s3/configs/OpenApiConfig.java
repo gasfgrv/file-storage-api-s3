762a97add4864a4f668b6cb4ffdb858bb0b74634
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-02 10:49:12 -0300

    Configuração do Swagger

---
+++ src/main/java/com/github/gasfgrv/storage/s3/configs/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.github.gasfgrv.storage.s3.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "File Storage api S3",
+        description = "Api para fazer upload/dowload de arquivos no S3",
+        version = "V1",
+        contact = @Contact(
+                name = "gasfgrv",
+                email = "gustavo_almeida11@hotmail.com",
+                url = "https://github.com/gasfgrv")
+))
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi logApi() {
+        return GroupedOpenApi.builder()
+                .group("api-v1")
+                .packagesToScan("com.github.gasfgrv.storage.s3.controller")
+                .pathsToMatch("/v1/**")
+                .build();
+    }
+
+}
