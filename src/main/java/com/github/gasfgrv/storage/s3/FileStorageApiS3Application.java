dd69630ee9dc3825d998c258645444ef9987153f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-02 14:51:31 -0300

    Remoção de import não utilizado

--- src/main/java/com/github/gasfgrv/storage/s3/FileStorageApiS3Application.java
+++ src/main/java/com/github/gasfgrv/storage/s3/FileStorageApiS3Application.java
@@ -2,7 +2,6 @@ package com.github.gasfgrv.storage.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class FileStorageApiS3Application {
