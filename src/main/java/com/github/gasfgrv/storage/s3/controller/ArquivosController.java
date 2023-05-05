7877be4e9c1240a970ffd78a03690ab6de026c87
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-04 21:23:56 -0300

    Correção na classe de controller para englobar todas as exceções

--- src/main/java/com/github/gasfgrv/storage/s3/controller/ArquivosController.java
+++ src/main/java/com/github/gasfgrv/storage/s3/controller/ArquivosController.java
@@ -4,22 +4,16 @@ import com.github.gasfgrv.storage.s3.dto.Resposta;
 import com.github.gasfgrv.storage.s3.exception.UploadException;
 import com.github.gasfgrv.storage.s3.model.Arquivo;
 import com.github.gasfgrv.storage.s3.service.IArquivosService;
-import java.io.IOException;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+import static org.springframework.http.MediaType.*;
 
 @RestController
 @RequestMapping("/v1/arquivos")
@@ -32,23 +26,21 @@ public class ArquivosController {
     public ResponseEntity<Resposta> upload(@RequestParam("nomeArquivo") String nomeArquivo,
                                            @RequestParam("arquivo") MultipartFile arquivo) {
         try {
-            var upload = service.upload(Arquivo
-                    .builder()
+            var upload = service.upload(Arquivo.builder()
                     .id(UUID.randomUUID())
                     .nome(nomeArquivo)
                     .tipo(arquivo.getContentType())
                     .dados(arquivo.getBytes())
                     .build());
 
-            var resposta = Resposta
-                    .builder()
+            var resposta = Resposta.builder()
                     .mensagem(upload)
                     .build();
 
             resposta.add(linkTo(methodOn(ArquivosController.class).dowload(nomeArquivo)).withRel("download"));
 
             return ResponseEntity.ok(resposta);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new UploadException(e);
         }
 
@@ -56,8 +48,7 @@ public class ArquivosController {
 
     @GetMapping(value = "/download", produces = APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<byte[]> dowload(@RequestParam("nomeArquivo") String nomeArquivo) {
-        return ResponseEntity.ok(service.dowload(Arquivo
-                .builder()
+        return ResponseEntity.ok(service.dowload(Arquivo.builder()
                 .nome(nomeArquivo)
                 .build()));
     }
